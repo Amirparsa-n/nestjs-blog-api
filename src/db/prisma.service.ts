0ae965d8401490c478685bc189b50a5965e13540
Amirparsa-n <amirparsanaghibi@gmail.com>
2025-12-01 20:07:09 +0330

    Update Prisma configuration and service for PostgreSQL adapter
    - Changed Prisma client provider from "prisma-client-js" to "prisma-client" in schema.prisma.
    - Updated PrismaService to use the new PrismaPg adapter for PostgreSQL, including connection string handling.
    - Added logging for successful database connection in PrismaService.
    - Created a new prisma.config.ts file to define Prisma configuration, including schema and migrations path.

--- src/db/prisma.service.ts
+++ src/db/prisma.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({
+      adapter,
+      log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
+  }
+
   async onModuleInit() {
     try {
       await this.$connect();
+      console.log('Prisma connected');
     } catch (error) {
       console.log('Failed to connect to the database', error);
       throw new Error('Database connection error');
